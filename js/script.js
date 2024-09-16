7dd879153f737d7b87e19894ec52547c96360ddf
Mahmoud Ismail <mahmoud.ahmadismail@gmail.com>
2024-09-16 09:16:55 -0300

    Correção JS

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ async function fetchUsers(usuario, senha) {
                 loginInfo.push(login)
                 localStorage.setItem("login", JSON.stringify(loginInfo))
 
-                window.location.href = 'home.html'
+                window.location.href = 'Home.html'
                 valida = 1
             }
         })
